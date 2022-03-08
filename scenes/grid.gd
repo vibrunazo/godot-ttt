93c232a4c9d5d55c4584996e01b7068528346411
Vandré Brunazo <vibrunazo@gmail.com>
2022-03-08 15:29:43 -0300

    build grid

--- scenes/grid.gd
+++ scenes/grid.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 var state = 'play'
 var ball_scene = preload("res://scenes/ball.tscn")
+var tile_scene = preload("res://scenes/tile.tscn")
+export var width = 3
+export var height = 3
+export var offset = 20
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -11,14 +15,14 @@ var ball_scene = preload("res://scenes/ball.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print('ready! grid!')
-	pass # Replace with function body.
+	build_grid()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if state == 'play':
-		pass
-	pass
+#func _process(delta):
+#	if state == 'play':
+#		pass
+#	pass
 
 func _input(event):
 	if event.is_action_pressed("ui_touch"):
@@ -27,3 +31,13 @@ func _input(event):
 		add_child(new_ball)
 		new_ball.position = event.position
 		
+func build_grid():
+	print('building le grid')
+	for x in width:
+		for y in width:
+			build_tile_at(200 + x * (80 + offset/2), 100 + y * (80 + offset/2))
+
+func build_tile_at(x, y):
+	var new_tile = tile_scene.instance()
+	new_tile.position = Vector2(x, y)
+	add_child(new_tile)
