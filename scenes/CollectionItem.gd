6768aaabce5bdfbc1891bd663db62b41c526e60f
Vandré Brunazo <vibrunazo@gmail.com>
2022-04-16 13:44:32 -0300

    update collection item tex and text from type

--- scenes/CollectionItem.gd
+++ scenes/CollectionItem.gd
@@ -6,4 +6,9 @@ export var ammount := 1
 
 
 func _ready():
-	pass
+	update()
+
+func update():
+	$HBoxContainer/Label.text = "x %d" % ammount
+	var tex = $"/root/Global".get_tex_from_type(type, level)
+	$HBoxContainer/TextureRect.texture = tex
--- scenes/grid.gd
+++ scenes/grid.gd
@@ -10,12 +10,7 @@ var turn := 0
 var next := 'rock'
 var piece_scene := preload("res://scenes/piece.tscn")
 var tile_scene := preload("res://scenes/tile.tscn")
-#var tex_dic := {
-#	'life': [preload("res://assets/seed03.png"), preload("res://assets/leaf02.png"), preload("res://assets/tree.png")],
-#	'rock': [preload("res://assets/rock01.png"), preload("res://assets/steel02.png")],
-#	'fire': [preload("res://assets/fire.png"), preload("res://assets/coal.png")]
-#}
-var tex_dic = {}
+var tex_dic: Dictionary = {}
 export var width := 3
 export var height := 3	
 export var offset := 20
@@ -253,9 +248,7 @@ func id_to_grid(id):
 	return Vector2(0, 0)
 	
 func get_tex_from_type(new_type: String, level: int = 0):
-	if (level >= tex_dic[new_type].size()):
-		level = tex_dic[new_type].size() - 1
-	return tex_dic[new_type][level]
+	return $"/root/Global".get_tex_from_type(new_type, level)
 
 func _on_TimerPause_timeout():
 	state = 'play'
